8d1dbe93699e76466c017794fe215c6a49cac36c
Sumaira Sanaullah <sumaira.sanaullah@hotmail.co.uk>
2023-02-03 17:24:14 +0000

    Tidying up

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
 import React, { useState, useEffect } from "react";
-import "./App.css";
+
 import "@aws-amplify/ui-react/styles.css";
-import { API } from "aws-amplify";
-import {
-  Button,
-  Flex,
-  Heading,
-  Text,
-  TextField,
-  View,
-  withAuthenticator,
-} from "@aws-amplify/ui-react";
 import { listNotes } from "./graphql/queries";
 import {
   createNote as createNoteMutation,
@@ -27,7 +17,7 @@ import {
   View,
   withAuthenticator,
 } from '@aws-amplify/ui-react';
-
+import "./App.css";
 
 const App = ({ signOut }) => {
   const [notes, setNotes] = useState([]);
@@ -107,39 +97,39 @@ const App = ({ signOut }) => {
       </View>
       <Heading level={2}>Current Notes</Heading>
       <View margin="3rem 0">
-      {notes.map((note) => (
-  <Flex
-    key={note.id || note.name}
-    direction="row"
-    justifyContent="center"
-    alignItems="center"
-  >
-    <Text as="strong" fontWeight={700}>
-      {note.name}
-    </Text>
-    <Text as="span">{note.description}</Text>
-    {note.image && (
-      <Image
-        src={note.image}
-        alt={`visual aid for ${notes.name}`}
-        style={{ width: 400 }}
-      />
-    )}
-    <Button variation="link" onClick={() => deleteNote(note)}>
-      Delete note
-    </Button>
-  </Flex>
-))}
+        {notes.map((note) => (
+          <Flex
+            key={note.id || note.name}
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Text as="strong" fontWeight={700}>
+              {note.name}
+            </Text>
+            <Text as="span">{note.description}</Text>
+            {note.image && (
+              <Image
+                src={note.image}
+                alt={`visual aid for ${notes.name}`}
+                style={{ width: 400 }}
+              />
+            )}
+            <Button variation="link" onClick={() => deleteNote(note)}>
+              Delete note
+            </Button>
+          </Flex>
+        ))}
       </View>
       <Button onClick={signOut}>Sign Out</Button>
       <View
-  name="image"
-  as="input"
-  type="file"
-  style={{ alignSelf: "end" }}
-/>
+        name="image"
+        as="input"
+        type="file"
+        style={{ alignSelf: "end" }}
+      />
     </View>
-    
+
   );
 };
 
